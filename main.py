2cc33acc7ad83388c0465ef7f826e63156c95476
manpreet-raina <manpreetsinghraina7@gmail.com>
2022-07-28 00:08:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ from xgboost import XGBClassifier
 
 
 def load_model(X, model):
-    #scaler_func = MinMaxScaler()
-
-    X = pd.read_csv("/Users/manpreetsingh/Loan Defaulter Prediction/ADT/read/X_test.csv")
-    y = pd.read_csv("/Users/manpreetsingh/Loan Defaulter Prediction/ADT/read/y_test.csv")
-    #X = scaler_func.transform(X)
-    print(X.columns)
-    X = np.array(X).astype(np.float32)
-    y = np.array(y).astype(np.float32)
-
     ml_model = load_model_from_db(model)
     y_pred = ml_model.predict(X)
     for i in y_pred:
@@ -28,5 +19,3 @@ if __name__ == '__main__':
     obj1 = pp1()
     df = obj1.pre_processing()
     preprocessed_dataframe()
-    #load_model(df, "xgb")
-    #load_model(df, "rf")
